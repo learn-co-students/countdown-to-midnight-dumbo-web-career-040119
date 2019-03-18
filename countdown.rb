9af89d58258a1e9b5563b83bd81c8c1b3fa2619f
Codey Pinero <codey.pinero@gmail.com>
2019-03-18 00:15:58 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(n = 10)
+  while n > 0
+        puts "#{n} SECOND(S)!"
+        n -= 1
+    end
+    return  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n = 5)
+  while n > 0
+        sleep 1
+        n -= 1
+    end
 end
