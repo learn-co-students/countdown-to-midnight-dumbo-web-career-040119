94c5eed41c127d62322c622aa05a083d3d7d5336
Codey Pinero <codey.pinero@gmail.com>
2019-03-24 20:46:00 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -2,15 +2,15 @@
 
 def countdown(n)
   while n > 0
-        puts "#{n} SECOND(S)!"
-        n -= 1
-    end
-    return "HAPPY NEW YEAR!"
+      puts "#{n} SECOND(S)!"
+      n -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(n)
   while n > 0
-        sleep 1
-        n -= 1
-    end
+      sleep 1
+      n -= 1
+  end
 end
